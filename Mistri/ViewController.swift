ba34b1f84fca024d129ed0eb58e0e885a7bcff69
adnianwe <amit.ninawe@maximess.com>
2023-02-06 14:47:48 +0530

    project initiated

---
+++ Mistri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mistri
+//
+//  Created by Amit Ninawe on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
